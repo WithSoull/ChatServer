7f200f9d8b44c96a2f65fe14443548bcf3dd96cb
Илья Гришин <withsoull@yadnex.ru>
2025-08-19 00:15:32 +0300

    update handler layer

--- internal/handler/chat/create_chat.go
+++ internal/handler/chat/create_chat.go
@@ -3,6 +3,7 @@ package chat
 import (
 	"context"
 
+	"github.com/WithSoull/ChatServer/internal/converter"
 	desc "github.com/WithSoull/ChatServer/pkg/chat/v1"
 )
 
@@ -10,9 +11,7 @@ func (h *Handler) CreateChat(ctx context.Context, req *desc.CreateChatRequest) (
 	chatId, err := h.service.CreateChat(
 		ctx,
 		req.GetSenderId(),
-		req.GetChatInfo().GetName(),
-		req.GetChatInfo().GetDescription(),
-		req.GetChatInfo().GetUserIds(),
+		converter.FromProtoToModelChatInfo(req.ChatInfo),
 	)
 
 	return &desc.CreateChatResponse{
